9a4743202421ac3e0125bfee8011404d713204a5
anto-belo <70717945+anto-belo@users.noreply.github.com>
2020-09-07 14:26:38 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PL_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PL_Lab1.dir/main.c.obj"
+  "PL_Lab1.exe"
+  "PL_Lab1.exe.manifest"
+  "PL_Lab1.pdb"
+  "libPL_Lab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PL_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
