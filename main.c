260e3b777c3f1ee5f458a709c0907d0a8a9ca78a
anto-belo <70717945+anto-belo@users.noreply.github.com>
2020-09-07 15:03:51 +0300

    Lab completed

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Input two numbers:\n");
+    int a, b;
+    scanf("%d %d", &a, &b); // NOLINT(cert-err34-c)
+
+    if (a <= b) a = 0;
+    printf("%d %d", a, b);
+
     return 0;
 }
